b29afab4f75939cd0eea733ba411c01d43532277
Syberia5910 <nicolas.lecaille@wanadoo.fr>
2024-09-13 11:19:12 +0200

    push midi

---
+++ exo/exercice8/src/main.js
@@ -0,0 +1,35 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
+const app = createApp(App)
+const router = createRouter(() => {
+    // Choix du mode naviguation
+    // CreatewebHistory :  classique
+    // createWebHashHistory : Naviguation avec des ancres
+    // createMemoryHistory : Tests et rendu coté serveur (SSR)
+    history: createWebHistory(),
+    // tableau d'oject contenant les routes
+    routes: [
+        {
+            path: "/",
+            name: "Home",
+            component: HomeView
+        },
+        {
+            path: "/news",
+            name: "News",
+            component: () => import('@/views/NewsView.vue') // Cette écriture permet de ne charger la page qu'en cas d'appel
+        },
+        {
+            path: "/contact",
+            name: "Contact",
+            component: () => import('@/views/ContactView.vue') // Cette écriture permet de ne charger la page qu'en cas d'appel
+        }
+    ];
+})
+app.use(router)
+app.mount('#app')
